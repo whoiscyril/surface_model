d347c10616f4369960b31e79e28ac5d4471700ba
unknown <cyrilyreal@gmail.com>
2023-08-24 22:57:53 +0100

    fixed derivatives on buckingham potential including clusters, pending tests for p1 cells

--- src/derivatives.cpp
+++ src/derivatives.cpp
@@ -38,26 +38,26 @@ UnitCell calc_forces(UnitCell unitcell_init)
         }
     }
 
-    double pot_derivx = 0.;
-    double pot_derivy = 0.;
-    double pot_derivz = 0.;
-    // int maxx = ceil(cutoff/a);
-    // int maxy = ceil(cutoff/b);
-    // int maxz = ceil(cutoff/c);
-    int maxx = 0;
-    int maxy = 0;
-    int maxz = 0;
+    // double pot_derivx = 0.;
+    // double pot_derivy = 0.;
+    // double pot_derivz = 0.;
+    int maxx = ceil(cutoff/a);
+    int maxy = ceil(cutoff/b);
+    int maxz = ceil(cutoff/c);
+    // int maxx = 0;
+    // int maxy = 0;
+    // int maxz = 0;
 
     Eigen::Vector3d rij;
     Eigen::Vector3d n;
     Eigen::Vector3d rijn;
 
+    Eigen::Vector3d pot_deriv;
+    pot_deriv.setZero();
+
     for (auto& elem1 : atoms)
     {
-        // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
-        pot_derivx = 0;
-        pot_derivy = 0;
-        pot_derivz = 0;
+        pot_deriv.setZero();
         // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
 
         for (auto& elem2 : atoms)
@@ -81,71 +81,66 @@ UnitCell calc_forces(UnitCell unitcell_init)
                                         ((pot.atom2_type == elem2.type) && (pot.atom2_label == elem2.label)) &&
                                         rij.norm() <= pot.cut_off2 && rij.norm() > 0.1)
                                 {
-                                    pot_derivx += (- elem1.x / rij.norm()) * expterm;
-                                    pot_derivy += (- elem1.y / rij.norm()) * expterm;
-                                    pot_derivz += (- elem1.z / rij.norm()) * expterm;
+                                    pot_deriv += -rij/rij.norm() * expterm;
                                     // std::cout << " Yes1" << " " << elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std::cout << pot.A <<" " << pot.rho << " " << pot.C << std::endl;
-                                    std::cout << (- elem1.x / rij.norm()) << std::endl;
+                                    // std::cout << (- elem1.x / rij.norm()) << std::endl;
 
                                 }
                                 else if (((pot.atom2_type == elem1.type) && (pot.atom2_label == elem1.label)) &&
                                          ((pot.atom1_type == elem2.type) && (pot.atom1_label == elem2.label)) &&
                                          rij.norm() <= pot.cut_off2 && rij.norm() > 0.1)
                                 {
-                                    pot_derivx += (- elem1.x / rij.norm()) * expterm;
-                                    pot_derivy += (- elem1.y / rij.norm()) * expterm;
-                                    pot_derivz += (- elem1.z / rij.norm()) * expterm;
+                                    pot_deriv += -rij/rij.norm() * expterm;
                                     // std::cout << " Yes2" << " " << elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
-                                                             std::cout << (- elem1.x / rij.norm()) << std::endl;
+                                                             // std::cout << (- elem1.x / rij.norm()) << std::endl;
 
                                 }
                                 // std::cout <<elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                             }
                             // std::cout <<elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                         }
-                        // else
-                        // {
-                        //     n << i * a, j * b, k * c;
-                        //     rijn << rij + n;
-                        //     for (const auto pot : unitcell_init.buckingham_potentials)
-                        //     {
-                        //         double expterm = (-pot.A / pot.rho) * exp(-rijn.norm()/pot.rho) + 6. * pot.C / pow(rijn.norm(), 7.);
+                        else
+                        {
+                            n << i * a, j * b, k * c;
+                            rijn << rij + n;
+                            for (const auto pot : unitcell_init.buckingham_potentials)
+                            {
+                                double expterm = (-pot.A / pot.rho) * exp(-rijn.norm()/pot.rho) + 6. * pot.C / pow(rijn.norm(), 7.);
 
-                        //         if (((pot.atom1_type == elem1.type) && (pot.atom1_label == elem1.label)) &&
-                        //                 ((pot.atom2_type == elem2.type) && (pot.atom2_label == elem2.label)) &&
-                        //                 rijn.norm() <= pot.cut_off2)
-                        //         {
-                        //             pot_derivx += (- elem1.x / rijn.norm()) * expterm;
-                        //             pot_derivy += (- elem1.y / rijn.norm()) * expterm;
-                        //             pot_derivz += (- elem1.z / rijn.norm()) * expterm;
-                        //             // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
-                        //             // std:: cout << rijn.norm() << std::endl;
+                                if (((pot.atom1_type == elem1.type) && (pot.atom1_label == elem1.label)) &&
+                                        ((pot.atom2_type == elem2.type) && (pot.atom2_label == elem2.label)) &&
+                                        rijn.norm() <= pot.cut_off2)
+                                {
+                                    pot_deriv += -rijn/rijn.norm() * expterm;
 
-                        //         }
-                        //         else if (((pot.atom2_type == elem1.type) && (pot.atom2_label == elem1.label)) &&
-                        //                  ((pot.atom1_type == elem2.type) && (pot.atom1_label == elem2.label)) &&
-                        //                  rijn.norm() <= pot.cut_off2)
-                        //         {
-                        //             pot_derivx += (- elem1.x / rijn.norm()) * expterm;
-                        //             pot_derivy += (- elem1.y / rijn.norm()) * expterm;
-                        //             pot_derivz += (- elem1.z / rijn.norm()) * expterm;
-                        //         }
-                        //     }
-                        // }
+                                    // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
+                                    // std:: cout << rijn.norm() << std::endl;
+
+                                }
+                                else if (((pot.atom2_type == elem1.type) && (pot.atom2_label == elem1.label)) &&
+                                         ((pot.atom1_type == elem2.type) && (pot.atom1_label == elem2.label)) &&
+                                         rijn.norm() <= pot.cut_off2)
+                                {
+                                    pot_deriv += -rijn/rijn.norm() * expterm;
+
+                                }
+                            }
+                        }
                     }
 
                 }
             }
             // std::cout <<elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
         }
-        elem1.fx = pot_derivx;
-        elem1.fy = pot_derivy;
-        elem1.fz = pot_derivz;
+        // elem1.fx = pot_derivx;
+        // elem1.fy = pot_derivy;
+        // elem1.fz = pot_derivz;
    	    // std::cout <<elem1.label << " " << elem2.label <<" " << elem1.fx << " " << elem1.fy << " " << elem1.fz << std::endl;
 
+        std::cout << pot_deriv[0] <<" " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
 
     }
 
