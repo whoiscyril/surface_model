f11ab7f663f662988501de49d85d27fe4361d545
Work <uccazxu@ucl.ac.uk>
2023-08-25 17:37:21 +0100

    implemented force calculations for electrostatics, pending test

--- src/derivatives.cpp
+++ src/derivatives.cpp
@@ -82,6 +82,7 @@ UnitCell calc_forces(UnitCell unitcell_init)
                                         rij.norm() <= pot.cut_off2 && rij.norm() > 0.1)
                                 {
                                     pot_deriv += -rij/rij.norm() * expterm;
+                                    std::cout <<elem1.label << " " << elem2.label <<" " << pot_deriv[0] << " " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
                                     // std::cout << " Yes1" << " " << elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std::cout << pot.A <<" " << pot.rho << " " << pot.C << std::endl;
@@ -93,9 +94,10 @@ UnitCell calc_forces(UnitCell unitcell_init)
                                          rij.norm() <= pot.cut_off2 && rij.norm() > 0.1)
                                 {
                                     pot_deriv += -rij/rij.norm() * expterm;
+                                    std::cout <<elem1.label << " " << elem2.label <<" " << pot_deriv[0] << " " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
                                     // std::cout << " Yes2" << " " << elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
-                                                             // std::cout << (- elem1.x / rij.norm()) << std::endl;
+                                    // std::cout << (- elem1.x / rij.norm()) << std::endl;
 
                                 }
                                 // std::cout <<elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
@@ -115,6 +117,7 @@ UnitCell calc_forces(UnitCell unitcell_init)
                                         rijn.norm() <= pot.cut_off2)
                                 {
                                     pot_deriv += -rijn/rijn.norm() * expterm;
+                                    std::cout <<elem1.label << " " << elem2.label <<" " << pot_deriv[0] << " " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
 
                                     // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
                                     // std:: cout << rijn.norm() << std::endl;
@@ -125,6 +128,7 @@ UnitCell calc_forces(UnitCell unitcell_init)
                                          rijn.norm() <= pot.cut_off2)
                                 {
                                     pot_deriv += -rijn/rijn.norm() * expterm;
+                                    std::cout <<elem1.label << " " << elem2.label <<" " << pot_deriv[0] << " " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
 
                                 }
                             }
@@ -133,14 +137,14 @@ UnitCell calc_forces(UnitCell unitcell_init)
 
                 }
             }
-            // std::cout <<elem1.label << " " << elem2.label <<" " << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
+            // std::cout <<elem1.label << " " << elem2.label <<" " << pot_deriv[0] << " " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
         }
-        // elem1.fx = pot_derivx;
-        // elem1.fy = pot_derivy;
-        // elem1.fz = pot_derivz;
-   	    // std::cout <<elem1.label << " " << elem2.label <<" " << elem1.fx << " " << elem1.fy << " " << elem1.fz << std::endl;
+        // elem1.fx = pot_deriv[0];
+        // elem1.fy = pot_deriv[1];
+        // elem1.fz = pot_deriv[2];
+        // std::cout << elem1.fx << " " << elem1.fy << " " << elem1.fz << std::endl;
 
-        std::cout << pot_deriv[0] <<" " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
+        // std::cout << pot_deriv[0] <<" " << pot_deriv[1] << " " << pot_deriv[2] << std::endl;
 
     }
 
@@ -150,5 +154,119 @@ UnitCell calc_forces(UnitCell unitcell_init)
     // }
 
     // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
+
+    //Now calculate electrostatic contributions to force
+    const double toeV = 14.39964390675221758120;
+    int natom = unitcell_init.coordinates_frac.size();
+    Eigen::Vector3d a1 = unitcell_init.lattice_vectors.row(0);
+    Eigen::Vector3d a2 = unitcell_init.lattice_vectors.row(1);
+    Eigen::Vector3d a3 = unitcell_init.lattice_vectors.row(2);
+
+    Eigen::Vector3d g1 = unitcell_init.reciprocal_vectors.row(0);
+    Eigen::Vector3d g2 = unitcell_init.reciprocal_vectors.row(1);
+    Eigen::Vector3d g3 = unitcell_init.reciprocal_vectors.row(2);
+
+    double V = unitcell_init.volume;
+
+    long double kappa = pow(((natom * 1. * M_PI*M_PI*M_PI)/ V / V), 1./6.);
+    double rcut = pow( -log(10E-17)/ (kappa * kappa),1./2.);
+    double kcut = 2.*kappa*sqrt((-log(10E-17)));
+
+    int nmax_x = ceil(rcut/a1.norm());
+    int nmax_y = ceil(rcut/a2.norm());
+    int nmax_z = ceil(rcut/a3.norm());
+    int kmax_x = ceil(kcut/g1.norm());
+    int kmax_y = ceil(kcut/g2.norm());
+    int kmax_z = ceil(kcut/g3.norm());
+
+    rij.setZero();
+    n.setZero();
+    rijn.setZero();
+
+        //Real part contribution Note calculated forces, not derivatves, hence negative sign in front of rijn.
+    Eigen::Vector3d real_deriv;
+    for ( auto& elem1 : atoms)
+    {   
+        real_deriv.setZero();
+        for ( auto& elem2 : atoms)
+        {
+            rij << elem1.x - elem2.x, elem1.y - elem2.y, elem1.z - elem2.z;
+            for (int i = -nmax_x; i<= nmax_x; ++i)
+            {
+                for (int j = -nmax_y; j <= nmax_y; ++j)
+                {
+                    for (int k = -nmax_z; k <= nmax_z; ++k)
+                    {
+                        n << i * a, j * b, k * c;
+                        rijn = rij + n;
+                        if (rijn.norm() <= rcut)
+                        {
+                            if (i == 0 && j == 0 && k ==0)
+                            {
+                                if (rijn.norm() > 0.1)
+                                {
+                                    real_deriv += toeV * (-rijn / rijn.norm()) * (-0.5 * elem1.q * elem2.q / (rijn.norm() * rijn.norm()))
+                                                    * ( (2. * kappa * rijn.norm() * exp(-kappa * kappa * rijn.norm() * rijn.norm())) / sqrt(M_PI) + erfc(kappa * rijn.norm()));
+                                } 
+                            }
+                            else
+                            {
+                                    real_deriv += toeV * (-rijn / rijn.norm()) * (-0.5 * elem1.q * elem2.q / (rijn.norm() * rijn.norm()))
+                                                    * ( (2. * kappa * rijn.norm() * exp(-kappa * kappa * rijn.norm() * rijn.norm())) / sqrt(M_PI) + erfc(kappa * rijn.norm())); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // elem1.fx += pot_deriv[0];
+        // elem1.fy += pot_deriv[1];
+        // elem1.fz += pot_deriv[2];
+    }
+    rijn.setZero();
+
+    //Reciprocal contribution, again forces, not derivatives;
+    Eigen::Vector3d reci_deriv;
+    Eigen::Vector3d kvecs;
+    kvecs.setZero();
+    for ( auto& elem1 : atoms)
+    {
+        reci_deriv.setZero();
+        for ( auto& elem2 : atoms)
+        {
+            rijn << (elem1.x - elem2.x), (elem1.y - elem2.y), (elem1.z - elem2.z);
+            for (int i = -kmax_x; i <= kmax_x; ++i)
+            {
+                for (int j = -kmax_y; j <= kmax_y; ++j)
+                {
+                    for (int k = -kmax_z; k <= kmax_z; ++k)
+                    {
+                        kvecs = i * g1, j * g2, k * g3;
+                        double kk = kvecs.norm() * kvecs.norm();
+
+                        if (kvecs.norm() <= kcut)
+                        {
+                            if (i == 0 && j == 0 && k == 0)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                reci_deriv += toeV * (-rijn / rijn.norm()) * ((2. * M_PI * elem1.q * elem2.q) / (V * kk))
+                                            * exp(-kk/4. * kappa * kappa) * -sin(kvecs.dot(rijn));
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        // elem1.fx += pot_deriv[0];
+        // elem1.fy += pot_deriv[1];
+        // elem1.fz += pot_deriv[2];
+        // std::cout << elem1.fx << " " << elem1.fy << " " << elem1.fz << std::endl;
+    }
+
+
     return unitcell_wforces;
 }
