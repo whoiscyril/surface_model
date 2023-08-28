e812e22dc91b8fd4951c86287abb9d5f083b0d29
unknown <cyrilyreal@gmail.com>
2023-08-28 20:45:20 +0100

    pending debug electrostatic derivatives

--- src/derivatives.cpp
+++ src/derivatives.cpp
@@ -108,133 +108,141 @@ UnitCell calc_forces(UnitCell unitcell_init)
                                 }
                             }
                         }
-                    }
+                    }    
 
                 }
             }
         }
-        std::cout << pot_deriv[0] * a <<" " << pot_deriv[1] * b << " " << pot_deriv[2] * c << std::endl;
+        // std::cout << pot_deriv[0] * a <<" " << pot_deriv[1] * b << " " << pot_deriv[2] * c << std::endl;
 
     }
 
-    // for (const auto& elem : unitcell_init.coordinates_cart)
-    // {
-    //     std::cout << elem.fx << " " << elem.fy << " " << elem.fz << std::endl;
-    // }
 
-    // std::cout << pot_derivx << " " << pot_derivy << " " << pot_derivz << std::endl;
 
-    // //Now calculate electrostatic contributions to force
-    // const double toeV = 14.39964390675221758120;
-    // int natom = unitcell_init.coordinates_frac.size();
-    // Eigen::Vector3d a1 = unitcell_init.lattice_vectors.row(0);
-    // Eigen::Vector3d a2 = unitcell_init.lattice_vectors.row(1);
-    // Eigen::Vector3d a3 = unitcell_init.lattice_vectors.row(2);
+    //Now calculate electrostatic contributions to force
+    const double toeV = 14.39964390675221758120;
+    int natom = unitcell_init.coordinates_frac.size();
+    Eigen::Vector3d a1 = unitcell_init.lattice_vectors.row(0);
+    Eigen::Vector3d a2 = unitcell_init.lattice_vectors.row(1);
+    Eigen::Vector3d a3 = unitcell_init.lattice_vectors.row(2);
 
-    // Eigen::Vector3d g1 = unitcell_init.reciprocal_vectors.row(0);
-    // Eigen::Vector3d g2 = unitcell_init.reciprocal_vectors.row(1);
-    // Eigen::Vector3d g3 = unitcell_init.reciprocal_vectors.row(2);
+    Eigen::Vector3d g1 = unitcell_init.reciprocal_vectors.row(0);
+    Eigen::Vector3d g2 = unitcell_init.reciprocal_vectors.row(1);
+    Eigen::Vector3d g3 = unitcell_init.reciprocal_vectors.row(2);
 
-    // double V = unitcell_init.volume;
+    double V = unitcell_init.volume;
 
-    // long double kappa = pow(((natom * 1. * M_PI*M_PI*M_PI)/ V / V), 1./6.);
-    // double rcut = pow( -log(10E-17)/ (kappa * kappa),1./2.);
-    // double kcut = 2.*kappa*sqrt((-log(10E-17)));
+    long double kappa = pow(((natom * 1. * M_PI*M_PI*M_PI)/ V / V), 1./6.);
+    double rcut = pow( -log(10E-17)/ (kappa * kappa),1./2.);
+    double kcut = 2.*kappa*sqrt((-log(10E-17)));
 
-    // int nmax_x = ceil(rcut/a1.norm());
-    // int nmax_y = ceil(rcut/a2.norm());
-    // int nmax_z = ceil(rcut/a3.norm());
-    // int kmax_x = ceil(kcut/g1.norm());
-    // int kmax_y = ceil(kcut/g2.norm());
-    // int kmax_z = ceil(kcut/g3.norm());
+    // double kappa = sqrt(0.220395);
+    // double rcut = 11.196897;
+    // double kcut = 4.935478;
 
-    // rij.setZero();
-    // n.setZero();
-    // rijn.setZero();
+    int nmax_x = ceil(rcut/a1.norm());
+    int nmax_y = ceil(rcut/a2.norm());
+    int nmax_z = ceil(rcut/a3.norm());
+    int kmax_x = ceil(kcut/g1.norm());
+    int kmax_y = ceil(kcut/g2.norm());
+    int kmax_z = ceil(kcut/g3.norm());
 
-    //     //Real part contribution Note calculated forces, not derivatves, hence negative sign in front of rijn.
-    // Eigen::Vector3d real_deriv;
-    // for ( auto& elem1 : atoms)
-    // {   
-    //     real_deriv.setZero();
-    //     for ( auto& elem2 : atoms)
-    //     {
-    //         rij << elem1.x - elem2.x, elem1.y - elem2.y, elem1.z - elem2.z;
-    //         for (int i = -nmax_x; i<= nmax_x; ++i)
-    //         {
-    //             for (int j = -nmax_y; j <= nmax_y; ++j)
-    //             {
-    //                 for (int k = -nmax_z; k <= nmax_z; ++k)
-    //                 {
-    //                     n << i * a, j * b, k * c;
-    //                     rijn = rij + n;
-    //                     if (rijn.norm() <= rcut)
-    //                     {
-    //                         if (i == 0 && j == 0 && k ==0)
-    //                         {
-    //                             if (rijn.norm() > 0.1)
-    //                             {
-    //                                 real_deriv += toeV * (-rijn / rijn.norm()) * (-0.5 * elem1.q * elem2.q / (rijn.norm() * rijn.norm()))
-    //                                                 * ( (2. * kappa * rijn.norm() * exp(-kappa * kappa * rijn.norm() * rijn.norm())) / sqrt(M_PI) + erfc(kappa * rijn.norm()));
-    //                             } 
-    //                         }
-    //                         else
-    //                         {
-    //                                 real_deriv += toeV * (-rijn / rijn.norm()) * (-0.5 * elem1.q * elem2.q / (rijn.norm() * rijn.norm()))
-    //                                                 * ( (2. * kappa * rijn.norm() * exp(-kappa * kappa * rijn.norm() * rijn.norm())) / sqrt(M_PI) + erfc(kappa * rijn.norm())); 
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     // elem1.fx += pot_deriv[0];
-    //     // elem1.fy += pot_deriv[1];
-    //     // elem1.fz += pot_deriv[2];
-    // }
-    // rijn.setZero();
+    rij.setZero();
+    n.setZero();
+    rijn.setZero();
 
-    // //Reciprocal contribution, again forces, not derivatives;
-    // Eigen::Vector3d reci_deriv;
-    // Eigen::Vector3d kvecs;
-    // kvecs.setZero();
-    // for ( auto& elem1 : atoms)
-    // {
-    //     reci_deriv.setZero();
-    //     for ( auto& elem2 : atoms)
-    //     {
-    //         rijn << (elem1.x - elem2.x), (elem1.y - elem2.y), (elem1.z - elem2.z);
-    //         for (int i = -kmax_x; i <= kmax_x; ++i)
-    //         {
-    //             for (int j = -kmax_y; j <= kmax_y; ++j)
-    //             {
-    //                 for (int k = -kmax_z; k <= kmax_z; ++k)
-    //                 {
-    //                     kvecs = i * g1, j * g2, k * g3;
-    //                     double kk = kvecs.norm() * kvecs.norm();
+        //Real part contribution Note calculated forces, not derivatves, hence negative sign in front of rijn.
+    Eigen::Vector3d real_deriv;
+    for ( auto& elem1 : atoms)
+    {   
+        real_deriv.setZero();
+        for ( auto& elem2 : atoms)
+        {
+            rij << elem1.x - elem2.x, elem1.y - elem2.y, elem1.z - elem2.z;
+            for (int i = -nmax_x; i<= nmax_x; ++i)
+            {
+                for (int j = -nmax_y; j <= nmax_y; ++j)
+                {
+                    for (int k = -nmax_z; k <= nmax_z; ++k)
+                    {
+                        n << i * a, j * b, k * c;
+                        rijn = rij + n;
+                        // Eigen::Vector3d prefactor = rijn/pow(rijn.norm(), 3.);
+                        // double erfcterm = erfc(k * rijn.norm()) + (2. * kappa / sqrt(M_PI) ) * rijn.norm() * exp(-kappa*kappa*rijn.norm() * rijn.norm());
+                        if (rijn.norm() <= rcut)
+                        {
+                            if (i == 0 && j == 0 && k ==0)
+                            {
+                                if (rijn.norm() > 0.1)
+                                {
+                                            real_deriv += rijn* toeV*(0.5*elem1.q*elem2.q)*((-2. * kappa/sqrt(M_PI))*(exp(-rijn.squaredNorm()*kappa * kappa)/rijn.norm())-(erfc(rijn.norm() * kappa )/rijn.squaredNorm()))/rijn.norm();
+                                    // real_deriv +=  (-rijn / rijn.norm()) * (-0.5 * elem1.q * elem2.q / (rijn.norm() * rijn.norm()))
+                                                    // * ( (2. * kappa * rijn.norm() * exp(-kappa * kappa * rijn.norm() * rijn.norm())) / sqrt(M_PI) + erfc(kappa * rijn.norm()));
+                                    // real_deriv += toeV * elem1.q * elem2.q * prefactor * erfcterm;
+                                } 
+                            }
+                            else
+                            {
+                                            real_deriv += rijn * toeV*(0.5*elem1.q*elem2.q)*((-2. * kappa/sqrt(M_PI))*(exp(-rijn.squaredNorm()*kappa * kappa)/rijn.norm())-(erfc(rijn.norm() * kappa )/rijn.squaredNorm()))/rijn.norm();
+                                    // real_deriv += (-rijn / rijn.norm()) * (-0.5 * elem1.q * elem2.q / (rijn.norm() * rijn.norm()))
+                                                    // * ( (2. * kappa * rijn.norm() * exp(-kappa * kappa * rijn.norm() * rijn.norm())) / sqrt(M_PI) + erfc(kappa * rijn.norm())); 
+                                    // real_deriv += toeV * elem1.q * elem2.q * prefactor * erfcterm;
 
-    //                     if (kvecs.norm() <= kcut)
-    //                     {
-    //                         if (i == 0 && j == 0 && k == 0)
-    //                         {
-    //                             continue;
-    //                         }
-    //                         else
-    //                         {
-    //                             reci_deriv += toeV * (-rijn / rijn.norm()) * ((2. * M_PI * elem1.q * elem2.q) / (V * kk))
-    //                                         * exp(-kk/4. * kappa * kappa) * -sin(kvecs.dot(rijn));
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
 
-    //     }
-    //     // elem1.fx += pot_deriv[0];
-    //     // elem1.fy += pot_deriv[1];
-    //     // elem1.fz += pot_deriv[2];
-    //     // std::cout << elem1.fx << " " << elem1.fy << " " << elem1.fz << std::endl;
-    // }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        std::cout <<  real_deriv[0] * a <<" " <<  real_deriv[1] * b << " " <<  real_deriv[2] * c << std::endl;
+
+    }
+    rijn.setZero();
+
+    //Reciprocal contribution, again forces, not derivatives;
+    Eigen::Vector3d reci_deriv;
+    Eigen::Vector3d kvecs;
+    kvecs.setZero();
+    for ( auto& elem1 : atoms)
+    {
+        reci_deriv.setZero();
+        for ( auto& elem2 : atoms)
+        {
+            rijn << (elem1.x - elem2.x), (elem1.y - elem2.y), (elem1.z - elem2.z);
+                                                        // std::cout << rijn[0] << " " << rijn[1] << " " << rijn[2] << std::endl;
+            for (int i = -kmax_x; i <= kmax_x; ++i)
+            {
+                for (int j = -kmax_y; j <= kmax_y; ++j)
+                {
+                    for (int k = -kmax_z; k <= kmax_z; ++k)
+                    {
+                        kvecs = i * g1 + j * g2 + k * g3;
+                        // std::cout << i << " " << j << " " << k << " " <<  kvecs[0] << " " << kvecs[1] << " " << kvecs[2] << std::endl;
+                        double kk = kvecs.norm() * kvecs.norm();
+
+                        if (kvecs.norm() <= kcut)
+                        {
+                            if (i == 0 && j == 0 && k == 0)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                reci_deriv += toeV *  kvecs * toeV *((2.*M_PI)/V)*elem1.q*elem2.q*exp(-0.25 * kk/kappa/kappa)/kk * -sin(kvecs.dot(rijn));
+                                // reci_deriv += -((2. * M_PI * elem1.q * elem2.q) / (V * kk)) * kvecs
+                                            // * exp(-kk/4. * kappa * kappa) * -sin(kvecs.dot(rijn));
+                                            // std::cout << elem1.label<< " "<< elem2.label << " " << rijn.norm() << std::endl;
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        std::cout << reci_deriv[0] * a <<" " << reci_deriv[1] * b << " " << reci_deriv[2] * c << std::endl;
+
+    }
 
 
     return unitcell_wforces;
